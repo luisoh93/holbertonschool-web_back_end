e043460f46b594521bd27b61da21f702a568c54e
luisoh93 <luis_oh@hotmail.com>
2023-09-02 12:58:25 -0700

    fixtask

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 """
-
 This module contains a type-annotated function.
-
 """
 
 
 def floor(n: float) -> int:
     """
-
     Args:
         n: The float.
 
     Returns:
         The floor of the float.
-    
     """
     return int(n)
